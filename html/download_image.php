d2c0986514fc3180a753d1b06b4dfd9333f0f6fe
Steve Gallo <smgallo@buffalo.edu>
2019-05-31 14:53:13 -0400

    Thwart invalid filename

--- html/download_image.php
+++ html/download_image.php
@@ -8,7 +8,13 @@ if ( ! isset($_GET['year']) || ! isset($_GET['name']) ) {
 $year = trim($_GET['year']);
 $name = trim($_GET['name']);
 
-$filename = "images/$year/raw/$name";
+// Ensure the filename falls under the expected directory and thwart injection attacks
+
+$filename = realpath("images/$year/raw/$name");
+if ( 0 !== strpos($filename, sprintf("%s/%s", getcwd(), 'images')) || ! is_file($filename) ) {
+    exit("Invalid image specification");
+}
+
 $imageInfo = getimagesize($filename);
 $mimeType = (array_key_exists('mime', $imageInfo) && ! empty($imageInfo['mime']) ? $imageInfo['mime'] : 'application/octet-stream');
 
